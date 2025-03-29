737066bcc445c8579acbd0b072c8e38ee80964a3
Amit Kumar Sahoo <72258273+AmitSahoo45@users.noreply.github.com>
2025-03-29 14:30:50 +0530

    Fix for Vercel : Update page.tsx

--- ui/app/images/page.tsx
+++ ui/app/images/page.tsx
@@ -1,1 +1,9 @@
-// empty file
+const ImagesPage: React.FC = () => {
+    return (
+        <div className="relative min-h-screen">
+            {/* Add your content here */}
+        </div>
+    );
+};
+
+export default ImagesPage;
